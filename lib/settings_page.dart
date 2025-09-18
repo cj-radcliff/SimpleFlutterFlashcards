8a8aab90f6fc985696c1c1e0a4555a1ae0b94543
Cory R <radcliffc@google.com>
2025-09-18 15:43:33 -0700

    Added the ability to kick off a network ping from the settings page (just calls the getQuestions end point over and over)

--- lib/settings_page.dart
+++ lib/settings_page.dart
@@ -1,5 +1,11 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'api_question_loader.dart';
+import 'package:dio/dio.dart';
 
 class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
@@ -11,21 +17,29 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   late TextEditingController _hostController;
   late TextEditingController _numberOfQuestionsController;
+  late TextEditingController _responseController;
   bool _useApiLoader = true;
-  final _myFocusNode = FocusNode();
+  bool _isTesting = false;
+  int _testCounter = 0;
+  Timer? _testTimer;
+  late ApiQuestionLoader _apiQuestionLoader;
 
   @override
   void initState() {
     super.initState();
     _hostController = TextEditingController();
     _numberOfQuestionsController = TextEditingController();
+    _responseController = TextEditingController();
     _loadSettings();
+    _apiQuestionLoader = ApiQuestionLoader(Dio());
   }
 
   @override
   void dispose() {
     _hostController.dispose();
     _numberOfQuestionsController.dispose();
+    _responseController.dispose();
+    _testTimer?.cancel();
     super.dispose();
   }
 
@@ -45,6 +59,33 @@ class _SettingsPageState extends State<SettingsPage> {
     await prefs.setInt('numberOfQuestions', int.parse(_numberOfQuestionsController.text));
   }
 
+  void _toggleApiTest() {
+    if (_isTesting) {
+      _testTimer?.cancel();
+      setState(() {
+        _isTesting = false;
+      });
+    } else {
+      setState(() {
+        _isTesting = true;
+        _testCounter = 0;
+      });
+      _testTimer = Timer.periodic(const Duration(milliseconds: 1000), (timer) {
+        _apiQuestionLoader.loadQuestions().then((questions) {
+          setState(() {
+            _testCounter++;
+            final responseJson = questions.map((q) => q.toMap()).toList();
+            _responseController.text = const JsonEncoder.withIndent('  ').convert(responseJson);
+          });
+        }).catchError((error) {
+          setState(() {
+            _responseController.text = error.toString();
+          });
+        });
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,20 +99,21 @@ class _SettingsPageState extends State<SettingsPage> {
             TextField(
               controller: _hostController,
               decoration: const InputDecoration(
-                labelText: 'Quiz Server Host',
+                labelText: 'Quiz Server Host (Note: Change this from localhost to your own IP)',
               ),
               enabled: true,
+              readOnly: false,
             ),
+
             TextField(
               controller: _numberOfQuestionsController,
               readOnly: false,
               enabled: true,
-              
               decoration: const InputDecoration(
                 labelText: 'Number of Questions',
               ),
               enableSuggestions: false,
-             // enableInteractiveSelection: true,
+              // enableInteractiveSelection: true,
               keyboardType: TextInputType.number,
             ),
             SwitchListTile(
@@ -90,6 +132,24 @@ class _SettingsPageState extends State<SettingsPage> {
                 Navigator.pop(context);
               },
               child: const Text('Save'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _toggleApiTest,
+              child: Text(_isTesting ? 'Running API Test: $_testCounter' : 'Start API Test'),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: TextField(
+                controller: _responseController,
+                maxLines: null,
+                expands: true,
+                readOnly: true,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'API Response',
+                ),
+              ),
             ),
           ],
         ),
